07f47aa873014fa3504030f5a061e334fee0fc8e
luizalvesot <alvesluizotavio23@gmail.com>
2023-11-01 22:53:16 -0300

    Projeto finalizado de acordo com a proposta

---
+++ ExercicioContaBancaria/ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿namespace ExercicioContaBancaria {
+    internal class ContaBancaria {
+
+        public double Saldo { get; private set; }
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+
+        public ContaBancaria(int Numero, string Titular) {
+            this.Numero= Numero;
+            this.Titular = Titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int Numero, string Titular, double depositoInicial) : this(Numero, Titular) {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.00;
+        }
+
+        public override string ToString() {
+            return "Conta: "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ ExercicioContaBancaria/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExercicioContaBancaria {
+    internal class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria cb;    
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+
+                cb = new ContaBancaria(numero, titular, depositoInicial);
+            } else{
+                cb = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("\nDados da conta: \n" + cb);
+
+            Console.Write("\n\n Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            cb.Deposito(quantia);
+            Console.WriteLine("\nDados da conta atualizados: \n" + cb);
+
+            Console.Write("\n\n Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            cb.Saque(quantia);
+            Console.WriteLine("\nDados da conta atualizados: \n" + cb);
+        }
+    }
+}
